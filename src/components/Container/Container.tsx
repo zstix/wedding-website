b5d9a19a81989ca28ce5d4c703c47acb3d812caa
Zack Stickles <zstickles@gmail.com>
2019-11-04 17:26:26 -0800

    Fixed container class names

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -4,8 +4,8 @@ interface ContainerProps {
   children: React.ReactNode;
   className?: string;
 }
-const Container: React.FC<ContainerProps> = ({ children, className }) => (
-  <div className={`zn-container ${className}`}>{children}</div>
+const Container: React.FC<ContainerProps> = ({ children, className = "" }) => (
+  <div className={`zn-container ${className}`.trim()}>{children}</div>
 );
 
 export default Container;
