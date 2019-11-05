d71a2b5923721f22e78b57fc2ecf520352fb1319
Zack Stickles <zstickles@gmail.com>
2019-11-04 17:26:00 -0800

    Added dummy text and typography styles

--- src/components/Pages/HomePage/HomePage.tsx
+++ src/components/Pages/HomePage/HomePage.tsx
@@ -4,17 +4,47 @@ import Container from "../../Container";
 
 const HomePage: React.FC = () => (
   <Container className="zn-page">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis metus at
-    risus vehicula gravida. Nulla facilisi. Cras et dolor pulvinar, lacinia nibh
-    id, tincidunt urna. Nam malesuada nec tellus vitae pellentesque. Integer
-    egestas magna ligula, eu iaculis arcu sollicitudin quis. Aliquam hendrerit
-    nulla vel mattis posuere. Vestibulum egestas convallis imperdiet. Maecenas
-    id augue ac nibh elementum pellentesque. Donec sodales nunc fringilla varius
-    accumsan. Aenean tempus varius lobortis. Nunc consequat ex vel justo sodales
-    malesuada. Class aptent taciti sociosqu ad litora torquent per conubia
-    nostra, per inceptos himenaeos. In efficitur quam et erat pharetra elementum
-    sit amet eu diam. Integer ligula risus, commodo at leo ac, fringilla
-    faucibus magna.
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis metus
+      at risus vehicula gravida. Nulla facilisi. Cras et dolor pulvinar, lacinia
+      nibh id, tincidunt urna. Nam malesuada nec tellus vitae pellentesque.
+      Integer egestas magna ligula, eu iaculis arcu sollicitudin quis. Aliquam
+      hendrerit nulla vel mattis posuere. Vestibulum egestas convallis
+      imperdiet. Maecenas id augue ac nibh elementum pellentesque. Donec sodales
+      nunc fringilla varius accumsan. Aenean tempus varius lobortis. Nunc
+      consequat ex vel justo sodales malesuada. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. In efficitur
+      quam et erat pharetra elementum sit amet eu diam. Integer ligula risus,
+      commodo at leo ac, fringilla faucibus magna.
+    </p>
+    <h1>This is a heading 1</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis metus
+      at risus vehicula gravida. Nulla facilisi. Cras et dolor pulvinar, lacinia
+      nibh id, tincidunt urna. Nam malesuada nec tellus vitae pellentesque.
+      Integer egestas magna ligula, eu iaculis arcu sollicitudin quis. Aliquam
+      hendrerit nulla vel mattis posuere. Vestibulum egestas convallis
+      imperdiet. Maecenas id augue ac nibh elementum pellentesque. Donec sodales
+      nunc fringilla varius accumsan. Aenean tempus varius lobortis. Nunc
+      consequat ex vel justo sodales malesuada. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. In efficitur
+      quam et erat pharetra elementum sit amet eu diam. Integer ligula risus,
+      commodo at leo ac, fringilla faucibus magna.
+    </p>
+    <h2>This is a heading 2</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis metus
+      at risus vehicula gravida. Nulla facilisi. Cras et dolor pulvinar, lacinia
+      nibh id, tincidunt urna. Nam malesuada nec tellus vitae pellentesque.
+      Integer egestas magna ligula, eu iaculis arcu sollicitudin quis. Aliquam
+      hendrerit nulla vel mattis posuere. Vestibulum egestas convallis
+      imperdiet. Maecenas id augue ac nibh elementum pellentesque. Donec sodales
+      nunc fringilla varius accumsan. Aenean tempus varius lobortis. Nunc
+      consequat ex vel justo sodales malesuada. Class aptent taciti sociosqu ad
+      litora torquent per conubia nostra, per inceptos himenaeos. In efficitur
+      quam et erat pharetra elementum sit amet eu diam. Integer ligula risus,
+      commodo at leo ac, fringilla faucibus magna.
+    </p>
   </Container>
 );
 
