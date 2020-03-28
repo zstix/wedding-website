5142fe775ad1be6f0af2208050b0a5fcae9cf520
Zack Stickles <zstickles@newrelic.com>
2020-03-27 20:27:02 -0700

    Basic layout

--- components/Carona/Carona.tsx
+++ components/Carona/Carona.tsx
@@ -1,7 +1,23 @@
 import * as React from 'react';
 
 const Carona = () => (
-  <div>This is the covid text</div>
+  <div className="zn-covid">
+    <div>
+      <h2>Dear Friends &amp; Family,</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget dui nec neque fermentum consequat eu quis nunc. Maecenas tincidunt nisi massa, ut aliquam tortor varius vitae. Cras ac felis bibendum, scelerisque augue in, placerat purus.
+      </p>
+      <p>
+        Etiam commodo interdum facilisis. Nunc quis tincidunt nibh, sit amet tempor augue. Sed dapibus, risus vitae pharetra scelerisque, felis ligula dictum neque, sit amet vestibulum metus ligula sed nisi. Suspendisse cursus ipsum ullamcorper lorem molestie, nec sodales purus elementum. Curabitur at dignissim diam. Morbi sit amet pulvinar massa, quis vestibulum ligula. Fusce placerat a purus eget iaculis.
+      </p>
+      <p>
+        Fusce in lacus nulla. Maecenas consectetur ex non facilisis dignissim.
+      </p>
+      <p>Zack &amp; Nina</p>
+    </div>
+
+    <img src="/images/us.jpg" />
+  </div>
 );
 
 export default Carona;
